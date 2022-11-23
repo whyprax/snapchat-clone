0495626e9f253cfa92cba301ebd18617989094f0
prathamdubey <prathamd67@gmail.com>
2022-11-23 22:00:06 +0530

    ChatView Added

--- src/components/Chats/ChatView.js
+++ src/components/Chats/ChatView.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from "react";
+import { CountdownCircleTimer } from "react-countdown-circle-timer";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { selectSelectedImage } from "../../features/appSlice";
@@ -22,6 +23,23 @@ const ChatView = () => {
   return (
     <div className="chatview">
       <img src={selectedImage} onClick={exit} alt="" />
+      <div className="chatview__timer">
+        <CountdownCircleTimer
+          isPlaying
+          duration={10}
+          strokeWidth={6}
+          size={50}
+          colors={["#004777", "#F7B801", "#A30000", "#A30000"]}
+          colorsTime={[10, 6, 3, 0]}
+        >
+          {({ remainingTime }) => {
+            if (remainingTime === 0) {
+              exit();
+            }
+            return remainingTime;
+          }}
+        </CountdownCircleTimer>
+      </div>
     </div>
   );
 };
